ab022c8a5107641cee4e7aee12dec85053a8fa4d
leandregrimmel <leandregrimmel@gmail.com>
2020-09-14 22:35:55 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    return (math.degrees)
 
 
 def to_celsius(temperature: float) -> float:
